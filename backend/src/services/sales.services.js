6c2157bfcfdb2fbac521dff12f226841203cba4a
TatiStoiev <tatistoiev@hotmail.com>
2024-04-02 19:08:01 -0300

    resolvi o problema doa validação do productId

--- backend/src/services/sales.services.js
+++ backend/src/services/sales.services.js
@@ -3,11 +3,11 @@ const validations = require('./validations/salesValidations');
 
 const createSales = async (array) => {
   const entries = Object.entries(validations);
-  const results = entries.map((entry) => {
+  const results = await Promise.all(entries.map(async (entry) => {
     const [attribute, validate] = entry;
-    const { isValid, error, type } = validate(array);
+    const { isValid, error, type } = await validate(array);
     return { attribute, isValid, error, type };
-  });
+  }));
 
   const errors = results.filter((r) => !r.isValid)
     .find((e) => e.error);
