8f4996e8453d605f6494b71fe20161a18febf8ef
TatiStoiev <tatistoiev@hotmail.com>
2024-04-02 21:06:30 -0300

    alterei a ordem da validação para que a quantity ocorra primeiro

--- backend/src/services/validations/salesValidations.js
+++ backend/src/services/validations/salesValidations.js
@@ -17,19 +17,6 @@ const productIdValidation = async (array) => {
 };
 
 const validations = {
-  productId: async (array) => {
-    const hasProductId = array.every((product) => 'productId' in product);
-    if (!hasProductId) {
-      return createNotValid('"productId" is required', 'REQUIRED');
-    }
-    if (hasProductId) {
-      const productsExists = await productIdValidation(array);
-      if (!productsExists) {
-        return createNotValid('Product not found', 'PRODUCT_NOTFOUND');
-      }
-      return { isValid: true };
-    }
-  },
   quantity: (array) => {
     const hasQuantity = array.every((product) => 'quantity' in product);
     const hasValidQuantity = array.every((product) => typeof product.quantity === 'number' 
@@ -42,7 +29,20 @@ const validations = {
     }
     return { isValid: true };
   },
- 
+
+  productId: async (array) => {
+    const hasProductId = array.every((product) => 'productId' in product);
+    if (!hasProductId) {
+      return createNotValid('"productId" is required', 'REQUIRED');
+    }
+    if (hasProductId) {
+      const productsExists = await productIdValidation(array);
+      if (!productsExists) {
+        return createNotValid('Product not found', 'PRODUCT_NOTFOUND');
+      }
+      return { isValid: true };
+    }
+  }, 
 };
 
 module.exports = validations;
