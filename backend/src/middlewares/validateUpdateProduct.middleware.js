f5ff27085aa4ee1d0f33366eb0cc2d9718e1c954
TatiStoiev <tatistoiev@hotmail.com>
2024-04-19 07:22:12 -0300

    fiz uma alteração no middleware validateQuantity

--- backend/src/middlewares/validateUpdateProduct.middleware.js
+++ backend/src/middlewares/validateUpdateProduct.middleware.js
@@ -1,11 +1,11 @@
 const validateQuantity = (req, res, next) => {
-  const quantity = req.body;
+  const { quantity } = req.body;
 
   if (quantity === '' || quantity === undefined) {
     return res.status(400).json({ message: '"quantity" is required' });
   }
 
-  if (quantity.length <= 0) {
+  if (Number.isNaN(quantity) || quantity <= 0) {
     return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
   }
 
